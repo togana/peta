5ac663e3c844eb6a691c2a3e8803147cd45799a6
togana <o.togana@gmail.com>
2016-05-28 20:30:52 +0900

    use to try

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const config = require('config');
 const router = new express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) =>
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: config.app.title })
 );
 
 module.exports = router;
